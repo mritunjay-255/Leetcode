ab8aa61bbf848d776fc0a33004b5244d0fc0ab48
Mritunjay Sahani <mrityunjayshn@gmail.com>
2025-09-18 15:33:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countDigits(int n) {
+        // code here
+        int cnt=0;
+        while(n!=0){
+            cnt++;
+            n/=10;
+        }
+        return cnt;
+    }
+}
